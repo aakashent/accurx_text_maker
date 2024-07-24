51718fb425734047a1ffaf09005f2e77009a63e1
aakashent <143434859+aakashent@users.noreply.github.com>
2024-07-24 22:31:45 +0100

    scriptv1.js
    Changed to not read the xml

--- script.js
+++ script.js
@@ -1,43 +1,26 @@
-window.onload = function() {
-    fetch('sitemap.xml')
-    .then(response => response.text())
-    .then(data => {
-        let parser = new DOMParser();
-        let xml = parser.parseFromString(data, "application/xml");
-        populateDropdown(xml);
-    });
-    document.getElementById('copyButton').addEventListener('click', copyToClipboard);
-};
+let linksData = {}; // This will be populated by the JSON data
 
-function populateDropdown(xml) {
-    let urls = xml.getElementsByTagName('loc');
-    let dropdown = document.getElementById('sitemapDropdown');
-    let urlArray = Array.from(urls);
+fetch('links_titles.json')
+.then(response => response.json())
+.then(data => {
+    linksData = data;
+    populateDropdown();
+});
 
-    // Sort URL array by text content alphabetically
-    urlArray.sort((a, b) => {
-        return a.textContent.localeCompare(b.textContent);
-    });
-
-    for (let i = 0; i < urlArray.length; i++) {
-        let option = document.createElement('option');
-        option.textContent = urlArray[i].textContent; // or use another child for a more descriptive text
-        option.value = urlArray[i].textContent;
+function populateDropdown() {
+    const dropdown = document.getElementById('sitemapDropdown');
+    Object.entries(linksData).forEach(([url, title]) => {
+        const option = document.createElement('option');
+        option.value = url;
+        option.textContent = title;  // Use the title for dropdown display
         dropdown.appendChild(option);
-    }
+    });
 }
 
 function displayLink() {
-    let dropdown = document.getElementById('sitemapDropdown');
-    let displayDiv = document.getElementById('linkDisplay');
-    displayDiv.innerHTML = `Please see the below link for a leaflet - <a href="${dropdown.value}">${dropdown.options[dropdown.selectedIndex].text}</a>`;
-}
-
-function copyToClipboard() {
-    let textToCopy = document.getElementById('linkDisplay').textContent;
-    navigator.clipboard.writeText(textToCopy).then(() => {
-        alert('Text copied to clipboard');
-    }).catch(err => {
-        alert('Failed to copy text: ', err);
-    });
+    const dropdown = document.getElementById('sitemapDropdown');
+    const url = dropdown.value;
+    const title = linksData[url];  // Retrieve the title using the URL
+    const displayDiv = document.getElementById('linkDisplay');
+    displayDiv.innerHTML = `Please see the below link for a leaflet - ${title}<br><a href="${url}">${url}</a>`;
 }
