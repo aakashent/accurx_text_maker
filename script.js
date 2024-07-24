8f4f8c684103df186708e1447e3aee0b4ce2c606
aakashent <143434859+aakashent@users.noreply.github.com>
2024-07-24 22:41:29 +0100

    Update script.js
    Debug added

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-let linksData = {}; // This will be populated by the JSON data
-
 fetch('links_titles.json')
-.then(response => response.json())
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+})
 .then(data => {
+    console.log("Data loaded successfully:", data);
     linksData = data;
     populateDropdown();
+})
+.catch(error => {
+    console.error('Error fetching JSON:', error);
 });
 
 function populateDropdown() {
     const dropdown = document.getElementById('sitemapDropdown');
     Object.entries(linksData).forEach(([url, title]) => {
+        console.log("Adding option:", title);
         const option = document.createElement('option');
         option.value = url;
         option.textContent = title;  // Use the title for dropdown display
